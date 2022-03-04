d0c11546337806c54754c9277173e685a9f1146c
AzureAD\kuanchen-wu <t108590006@ntut.org.tw>
2022-03-04 09:50:06 +0800

    GitTutorial-7B

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -3,14 +3,21 @@
 #include <cstdlib>
 #include <ctime>
 
-bool Guess(int number) {
-    return false;
+bool Guess(int number){
+	static int target = -1;
+	srand(time(NULL));
+	if (target == -1){
+		target = rand() % 100 + 1;
+	}
+	if (number = target)
+		return true;
+	return false;
 }
-
-int main() {
-    int guess;
-    do {
-        std::cin >> guess;
-    } while (!Guess(guess));
-    return 0;
+int main()
+{
+	int guess;
+	do {
+		std::cin >> guess;
+	} while (!Guess(guess));
+	return 0;
 }
