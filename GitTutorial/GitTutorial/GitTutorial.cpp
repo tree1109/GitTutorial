27ba752b90cbe61bb9aa885bf0fe30d4ba6d68e4
tree1109 <55215480+tree1109@users.noreply.github.com>
2022-03-04 09:52:17 +0800

    完成GitTutorial 8A

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -10,6 +10,7 @@ bool Guess(int number) {
 int main() {
     int guess;
     do {
+        std::cout << "Choose a number between 1 - 100 :";
         std::cin >> guess;
     } while (!Guess(guess));
     return 0;
